7b1f0612b70336d085a7e5da0f262fec48ae211b
fumiaki1108 <pasocon4649@gmail.com>
2020-11-17 19:56:59 +0900

    デプロイ

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root  'users#index'
+  root "items#index"
+#items_controllerを作ってindexアクションを定義→items／indexのビューファイルも作る→routes.rbでルートパスに指定する
 end
