de5fd9c41c306f8a3c355deb86b861914fe93ce8
fumiaki1108 <pasocon4649@gmail.com>
2020-11-18 20:27:55 +0900

    ユーザー管理機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,4 +4,23 @@ class ItemsController < ApplicationController
   def index
   end
 
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+
 end
