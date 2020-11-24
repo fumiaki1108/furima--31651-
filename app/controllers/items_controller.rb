9bb5d35d65d4bdc7c481b7071a5d078a810157e2
fumiaki1108 <pasocon4649@gmail.com>
2020-11-24 17:18:01 +0900

    ルーティングの設定とアクションの作成等

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,8 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :set_item, only: [:show, :edit]
+  before_action :move_to_index, only: [:edit]
+
   def index
     @items = Item.order(id: "DESC")
   end
@@ -19,11 +22,20 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
+
   end
 
   def edit
 
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
@@ -34,6 +46,16 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:image, :name, :info, :category_id, :sales_status_id, :shipping_fee_id,:prefecture_id, :scheduled_delivery_id, :price, :user_id).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def move_to_index
+    unless current_user == @item.user
+      redirect_to root_path
+    end
   end
 
 end
