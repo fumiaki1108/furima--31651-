decf10d549da25bea2198725abe385a70dbb4a28
fumiaki1108 <pasocon4649@gmail.com>
2020-11-27 19:51:59 +0900

    orderコントローラー編集

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   devise_for :users
   root "items#index"
   resources :items, only: [:new, :create, :show, :edit, :update, :destroy]
+  resources :orders, only: [:index, :create]
 end
